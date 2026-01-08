e101d98b25ddb4e1d364c55cbb16e39d10a30676
Rok Mušič <rokmusic0@gmail.com>
2026-01-08 17:02:56 +0100

    Improve bombwalker to only pick movements from legal actions

--- agents/bombwalker/agent.py
+++ agents/bombwalker/agent.py
@@ -1,17 +1,23 @@
-from typing import Union
-from game_state import GameState
 import asyncio
-import random
+import logging
 import os
+import random
 import time
 
-uri = os.environ.get(
-    'GAME_CONNECTION_STRING') or "ws://127.0.0.1:3000/?role=agent&agentId=agentId&name=defaultName"
+from game_state import GameState
+from types_ import ActionType, UnitState
+from types_ import GameState as TypedGameState
 
-actions = ["up", "down", "left", "right"]
+uri = (
+    os.environ.get("GAME_CONNECTION_STRING")
+    or "ws://127.0.0.1:3000/?role=agent&agentId=agentId&name=defaultName"
+)
+
+logger = logging.getLogger(__name__)
+logging.basicConfig(level=logging.INFO)
 
 
-class Agent():
+class Agent:
     def __init__(self):
         self._client = GameState(uri)
 
@@ -25,26 +31,47 @@ class Agent():
         ]
         loop.run_until_complete(asyncio.wait(tasks))
 
+    def get_random_legal_action(
+        self, game_state: TypedGameState, unit: UnitState
+    ) -> ActionType:
+        all_legal_actions = game_state.legal_actions(unit)
+        # only consider movements (and noop) to prevent self-destruction
+        desired_legal_actions = [
+            action
+            for action in all_legal_actions
+            if action
+            in {
+                ActionType.NOOP,
+                ActionType.UP,
+                ActionType.DOWN,
+                ActionType.LEFT,
+                ActionType.RIGHT,
+            }
+        ]
+        assert len(desired_legal_actions) > 0, (
+            "There should always be a legal action, NOOP at least."
+        )
+        return random.choice(desired_legal_actions)
 
-    async def _on_game_tick(self, tick_number, game_state):
-
-        # get my units
-        my_agent_id = game_state.get("connection").get("agent_id")
-        my_units = game_state.get("agents").get(my_agent_id).get("unit_ids")
+    async def _on_game_tick(self, tick_number, _game_state):
+        game_state = TypedGameState.from_dict(_game_state)
 
         # send each unit a random action
-        for unit_id in my_units:
+        for unit in game_state.my_alive_units:
+            action = self.get_random_legal_action(game_state, unit)
+            logger.info(
+                f"Tick {tick_number}: Unit {unit.unit_id} takes action {action}"
+            )
 
-            action = random.choice(actions)
-
-            if action in ["up", "left", "right", "down"]:
-                await self._client.send_move(action, unit_id)
+            if action == ActionType.NOOP:
+                continue  # do nothing
             else:
-                print(f"Unhandled action: {action} for unit {unit_id}")
+                action_packet = action.to_action_packet(unit.unit_id, game_state)
+                await self._client._send(action_packet.to_dict())
 
 
 def main():
-    for i in range(0,10):
+    for i in range(0, 10):
         while True:
             try:
                 Agent()
